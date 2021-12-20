495d232980a58ced31a725e047a67d962a6a5a32
prasadbanuka <banuka.mawelage@gmail.com>
2021-12-20 19:49:04 +0530

    footer change

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -19,7 +19,7 @@
                         <i class="bi bi-telephone-fill text-white"></i>
                     </div>
                     <div class="col-10 pl-0">
-                        <p class="text-white">+94 777000000</p>
+                        <p class="text-white">0777531545</p>
                     </div>
                 </div>
 
@@ -28,7 +28,7 @@
                         <i class="bi bi-envelope-fill text-white"></i>
                     </div>
                     <div class="col-10 pl-0">
-                        <p class="text-white">info@eshop.com</p>
+                        <p class="text-white">anurcompany@yahoo.com</p>
                     </div>
                 </div>
 
@@ -37,27 +37,27 @@
                         <i class="bi bi-geo-alt-fill text-white"></i>
                     </div>
                     <div class="col-10 pl-0">
-                        <p class="text-white">587/ C NUGEGODA, COLOMBO 5</p>
+                        <p class="text-white">No.285, Galloluwa, <br> Minuwangoda, Srilanka</p>
                     </div>
                 </div>
             </div>
 
             <div class="col-12 col-md links quick" style="padding-left: 3rem;">
                 <h6 class="fw-bold mt-4 mt-md-5 mb-4">Quick Links</h6>
-                <a href="" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Home</a> <br>
-                <a href="" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Explore</a> <br>
-                <a href="" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Training</a> <br>
-                <a href="" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">About Us</a>
+                <a href="#" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Home</a> <br>
+                <a href="#products" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Products</a> <br>
+                <a href="#about-us" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">About Us</a> <br>
+                <a href="#contact" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Contact Us</a>
             </div>
 
-            <div class="col-12 col-md links">
+            <!-- <div class="col-12 col-md links">
                 <h6 class="fw-bold mt-4 mt-md-5 mb-4">Important</h6>
                 <a href="" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Terms & Conditions</a> <br>
                 <a href="" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Become a Partner</a> <br>
                 <a href="" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Cookie Policy</a> <br>
                 <a href="" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">Privacy Policy</a> <br>
                 <a href="" class="mb-3 text-decoration-none text-white ms-4 ms-md-0">FAQ</a>
-            </div>
+            </div> -->
             <div class="col-12 col-md social">
                 <h6 class="fw-bold mt-4 mt-md-5 mb-4">Follow Us</h6>
 
